588ad0827e0afc8a7c3a974e0a8e494eb3083fff
vicumg <victor.skripnyuk@gmail.com>
2020-05-26 15:40:30 +0300

    add noindex functions

--- www/admin/controller/extension/module/ocfilter_value_seo.php
+++ www/admin/controller/extension/module/ocfilter_value_seo.php
@@ -7,6 +7,7 @@ class ControllerExtensionModuleOcfilterValueSeo extends Controller {
 
         $data['ocfilterOptions'] =$this->model_extension_module_ocfilter_value_seo->getOcfilterOptions();
         $data['ocf_descriptions'] =$this->model_extension_module_ocfilter_value_seo->getOcValuesSeo($data['categoryId']);
+        $this->document->addScript('view/javascript/oc_filter_seo_text.js');
 
         return $this->load->view('extension/module/ocfilter_value_seo', $data);
     }
